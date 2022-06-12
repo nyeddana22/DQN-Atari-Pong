99c50d721c3434e785713dedbb5590b496d7ae5e
nyeddana22 <91582142+nyeddana22@users.noreply.github.com>
2022-06-11 20:31:05 -0700

    Update DQN_with_PER.py

--- DQN_with_PER.py
+++ DQN_with_PER.py
@@ -12,6 +12,7 @@ USE_CUDA = torch.cuda.is_available()
 Variable = lambda *args, **kwargs: autograd.Variable(*args, **kwargs).cuda() if USE_CUDA else autograd.Variable(*args, **kwargs)
 
 class QLearner(nn.Module):
+    #Define CNN and feed forward layers of DQN
     def __init__(self, env, num_frames, batch_size, gamma, replay_buffer):
         super(QLearner, self).__init__()
         self.batch_size = batch_size
@@ -35,33 +36,34 @@ class QLearner(nn.Module):
             nn.Linear(512, self.num_actions)
         )
 
-    def forward(self, x):
-        x = self.features(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc(x)
-        return x
+    def forward(self, state):
+        state = self.features(state)
+        state = state.view(state.size(0), -1)
+        qtable = self.fc(state)
+        return qtable
         
 
     def feature_size(self):
             return self.features(autograd.Variable(torch.zeros(1, *self.input_shape))).view(1, -1).size(1)
-
+    
+    #Define function to choose actions given a state
     def act(self, state, epsilon):
-        # epsilon = 1.0  # just for testing random agent
         if random.random() > epsilon:
+            #If state is known, choose action corresponding to the max Q value from the Q table
             state = Variable(torch.FloatTensor(np.float32(state)).unsqueeze(0), requires_grad=True)
-            # TODO: Given state, you should write code to get the Q value and chosen action
             Q_val = self.forward(state)
             _, action = Q_val.max(1)
         else:
+            #If state is not known, choose a random action from the env action space to discover new states
             action = random.randrange(self.env.action_space.n)
         return action
 
     def copy_from(self, target):
         self.load_state_dict(target.state_dict())
 
-
+#Define TD loss function
 def compute_td_loss(model, target_model, batch_size, gamma, replay_buffer):
-#     state, action, reward, next_state, done = replay_buffer.sample(batch_size)
+
     state, action, reward, next_state, done, sample_indices, weights = replay_buffer.sample(batch_size, beta)
     state = Variable(torch.FloatTensor(np.float32(state)).squeeze(1))
     next_state = Variable(torch.FloatTensor(np.float32(next_state)).squeeze(1), requires_grad=True)
@@ -69,16 +71,15 @@ def compute_td_loss(model, target_model, batch_size, gamma, replay_buffer):
     reward = Variable(torch.FloatTensor(reward))
     done = Variable(torch.FloatTensor(done))
     weights = Variable(torch.FloatTensor(weights))
-    # implement the loss function here
-
+    
+    #Acquire current and next state Q values from the Q table
     curr_qvals = model.forward(state)
     next_qvals = target_model.forward(next_state)
     curr_qval = curr_qvals.gather(1, action.unsqueeze(-1)).squeeze(-1)
     next_qval = next_qvals.max(1)[0]
     expected_qval = reward + gamma * next_qval * (1 - done)
-#     loss = torch.nn.functional.mse_loss(expected_qval,curr_qval)
 
-    #MSE Loss calculation for PER 
+    #MSE Loss calculation for PER by multiplying weights for priorities
     loss  = (curr_qval - expected_qval.detach()).pow(2) * weights
     prios = loss + 1e-5
     replay_buffer.update_priorities(sample_indices, prios.data.cpu().numpy())
@@ -89,8 +90,6 @@ class PrioritizedReplayBuffer(object):
     def __init__(self, capacity, alpha = 0.6):
         self.capacity = capacity
         self.buffer = deque(maxlen=capacity)
-
-        #PER
         self.alpha = alpha
         self.position = 0
         self.priorities = np.zeros((capacity,), dtype=np.float32)
@@ -109,18 +108,20 @@ class PrioritizedReplayBuffer(object):
         self.position = (self.position + 1) % self.capacity
 
     def sample(self, batch_size, beta=0.4):
-        # TODO: Randomly sampling data with specific batch size from the buffer
-
+        #Acquire current and next state Q values from the Q table
+        
         if len(self.buffer) == self.capacity:
             prios = self.priorities
         else:
             prios = self.priorities[:self.position]
-    
+        
+        #calculate weighted probabilities to use in sampling
         probs = prios ** self.alpha
         probs /= probs.sum()
         total = len(self.buffer)
         sample_indices = np.random.choice(len(self.buffer),batch_size, p=probs)
-
+        
+        #assign weights to the samples 
         weights = (total * probs[sample_indices]) ** (-beta)
         weights /= weights.max()
         weights = np.array(weights, dtype=np.float32)
@@ -138,9 +139,8 @@ class PrioritizedReplayBuffer(object):
           done.append(self.buffer[i][4])
 
         return state, action, reward, next_state, done, sample_indices, weights
-        # return None # just for testing random agent
-        #return state, action, reward, next_state, done
-
+    
+    #update priorities after calculating the loss
     def update_priorities(self, batch_indices, batch_priorities):
         for idx, prio in zip(batch_indices, batch_priorities):
             self.priorities[idx] = prio
