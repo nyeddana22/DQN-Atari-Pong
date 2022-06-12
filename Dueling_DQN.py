8ceba41a904850d9700b6b1510c8a97a707c960a
nyeddana22 <91582142+nyeddana22@users.noreply.github.com>
2022-06-11 20:37:55 -0700

    Update Dueling_DQN.py

--- Dueling_DQN.py
+++ Dueling_DQN.py
@@ -1,4 +1,3 @@
-## dqn.py (Modified as Random Agent)
 from collections import deque
 import numpy as np
 import matplotlib
@@ -12,6 +11,7 @@ USE_CUDA = torch.cuda.is_available()
 Variable = lambda *args, **kwargs: autograd.Variable(*args, **kwargs).cuda() if USE_CUDA else autograd.Variable(*args, **kwargs)
 
 class QLearner(nn.Module):
+    #Dueling DQN Architecture Implementation
     def __init__(self, env, num_frames, batch_size, gamma, replay_buffer):
         super(QLearner, self).__init__()
         self.batch_size = batch_size
@@ -21,8 +21,6 @@ class QLearner(nn.Module):
         self.env = env
         self.input_shape = self.env.observation_space.shape
         self.num_actions = self.env.action_space.n
-
-    #Dueling DQN Architecture Implementation
 
         self.conv_nn = nn.Sequential(
             nn.Conv2d(self.input_shape[0], 32, kernel_size=8, stride=4),
@@ -49,47 +47,48 @@ class QLearner(nn.Module):
 			nn.Linear(512, 1)
         )
 
-    def forward(self, x):
-        batch_size = x.size()[0]
-        cnn_output = self.conv_nn(x).view(batch_size, -1)
+    def forward(self, state):
+        batch_size = state.size()[0]
+        cnn_output = self.conv_nn(state).view(batch_size, -1)
         value = self.linear_value(cnn_output)
         actions = self.linear_actions(cnn_output)
         return value + actions  - torch.mean(actions, dim=1, keepdim=True)
 
     def feature_size(self):
             return self.features(autograd.Variable(torch.zeros(1, *self.input_shape))).view(1, -1).size(1)
-
+    
+    #Define function to choose actions given a state
     def act(self, state, epsilon):
-        # epsilon = 1.0  # just for testing random agent
         if random.random() > epsilon:
+	    #If state is known, choose action corresponding to the max Q value from the Q table
             state = Variable(torch.FloatTensor(np.float32(state)).unsqueeze(0), requires_grad=True)
-            # TODO: Given state, you should write code to get the Q value and chosen action
             Q_val = self.forward(state)
             _, action = Q_val.max(1)
         else:
+	    #If state is not known, choose a random action from the env action space to discover new states
             action = random.randrange(self.env.action_space.n)
         return action
 
     def copy_from(self, target):
         self.load_state_dict(target.state_dict())
 
-
+#Define TD loss function
 def compute_td_loss(model, target_model, batch_size, gamma, replay_buffer):
+	
     state, action, reward, next_state, done = replay_buffer.sample(batch_size)
-    # state, action, reward, next_state, done, sample_indices, weights = replay_buffer.sample(batch_size, beta)
     state = Variable(torch.FloatTensor(np.float32(state)).squeeze(1))
     next_state = Variable(torch.FloatTensor(np.float32(next_state)).squeeze(1), requires_grad=True)
     action = Variable(torch.LongTensor(action))
     reward = Variable(torch.FloatTensor(reward))
     done = Variable(torch.FloatTensor(done))
-    weights = Variable(torch.FloatTensor(weights))
-    # implement the loss function here
 
+    #Acquire current and next state Q values from the Q table
     curr_qvals = model.forward(state)
     next_qvals = target_model.forward(next_state)
     curr_qval = curr_qvals.gather(1, action.unsqueeze(-1)).squeeze(-1)
     next_qval = next_qvals.max(1)[0]
     expected_qval = reward + gamma * next_qval * (1 - done)
+    #MSE loss function provides best value; can also try huber loss
     loss = torch.nn.functional.mse_loss(expected_qval,curr_qval)
 
     return loss
@@ -104,7 +103,7 @@ class ReplayBuffer(object):
         self.buffer.append((state, action, reward, next_state, done))
 
     def sample(self, batch_size):
-        # TODO: Randomly sampling data with specific batch size from the buffer
+        # Randomly sample data given a specific batch size from the buffer
         rand_data_indices = np.random.choice(len(self.buffer),batch_size)
         state = []
         action = []
@@ -119,7 +118,6 @@ class ReplayBuffer(object):
           done.append(self.buffer[i][4])
 
         return state, action, reward, next_state, done
-        # return None # just for testing random agent
         
     def __len__(self):
         return len(self.buffer)
